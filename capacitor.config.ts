aee356feddaf71e62e65457ad1b7455ef2e5b6cd
* <b4130478-web>
2026-06-27 22:27:52 +0300

    Fix: Migrate configuration file from TS to native JSON

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.b4130478.safemp3player',
-  appName: 'safe mp3 playergithub.com.b4130478-web.com.safemp3player',
-  webDir: 'build'
-};
-
-export default config;
