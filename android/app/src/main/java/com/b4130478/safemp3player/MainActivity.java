319aa21ab3dd4466c3de4ba59352eedc3e889f33
* <b4130478-web>
2026-06-27 20:51:25 +0300

    Add Capacitor Android setup

---
+++ android/app/src/main/java/com/b4130478/safemp3player/MainActivity.java
@@ -0,0 +1,5 @@
+package com.b4130478.safemp3player;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.b4130478.safemp3player',
+  appName: 'safe mp3 playergithub.com.b4130478-web.com.safemp3player',
+  webDir: 'build'
+};
+
+export default config;
